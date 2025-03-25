9e56e523376dc1291f8aa58fddd7d68493739ef4
itwqt-oraya <nicole.oraya.cics@ust.edu.ph>
2025-03-25 09:05:25 +0800

    Fix: use env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
-import 'package:gym_tracker_flutter/routes/app_router.dart';
+  import 'package:gym_tracker_flutter/routes/app_router.dart';
+  import 'package:firebase_core/firebase_core.dart';
+  import 'package:provider/provider.dart';
+  import 'package:gym_tracker_flutter/services/auth_service.dart';
+  import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+  void main() async {
+    await dotenv.load(fileName: ".env");
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MainApp());
+  }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
